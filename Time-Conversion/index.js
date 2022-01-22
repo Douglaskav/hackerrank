728eff3602bee8f6ae2ed804ce27265eb9c8b72a
Douglas Nicolas <dnmsmpvp@gmail.com>
2022-01-22 17:57:01 -0300

    Time-Conversion

--- Time-Conversion/index.js
+++ Time-Conversion/index.js
@@ -1,3 +1,10 @@
+/**
+ * @author Douglas Nicolas(douglaskav)
+ * [Time-Conversion](https://www.hackerrank.com/challenges/time-conversion/problem)
+ */
+
+
+
 function timeConversion(s) {
   let timeset = s.substr(s.length - 2, s.length);
 
